7b231dccfd46e5d518113d7d97d7f76d168bcfb7
Devil666face <artem1999k@gmail.com>
2023-07-01 23:06:18 +0300

    fix back page

--- config/static/js/main.js
+++ config/static/js/main.js
@@ -1,8 +1,8 @@
 function setTheme(event) {
 	var element = event.target;
 	var navbar = document.getElementById('navbar');
-	navbar.setAttribute('data-bs-theme',element.getAttribute('data-bs-theme-value'))
+	navbar.setAttribute('data-bs-theme',element.getAttribute('data-bs-theme-value'));
 }
 for (id in ['light', 'dark', 'auto']) {
-	document.getElementById('bd-theme-'+id).addEventListener('click', setTheme)
+	document.getElementById('bd-theme-'+id).addEventListener('click', setTheme);
 }
