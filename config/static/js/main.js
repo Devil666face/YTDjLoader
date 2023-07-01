60b0d655cef7f3cd1d967de2e0b791d34a5f58ec
Devil666face <artem1999k@gmail.com>
2023-07-01 20:16:49 +0300

    add logger

--- config/static/js/main.js
+++ config/static/js/main.js
@@ -3,7 +3,6 @@ function setTheme(event) {
 	var navbar = document.getElementById('navbar');
 	navbar.setAttribute('data-bs-theme',element.getAttribute('data-bs-theme-value'))
 }
-var buttonsId = ['light','dark', 'auto']
-for (id in buttonsId) {
+for (id in ['light', 'dark', 'auto']) {
 	document.getElementById('bd-theme-'+id).addEventListener('click', setTheme)
 }
--- main.py
+++ main.py
@@ -4,13 +4,14 @@ from waitress import serve
 from config.wsgi import application
 from dotenv import load_dotenv
 from app.apps import AppConfig
+from paste.translogger import TransLogger
 
 load_dotenv()
 
 if __name__ == "__main__":
     host = os.getenv("HOST", "127.0.0.1")
     port = os.getenv("PORT", "8000")
-    serve(application, host=host, port=port)
+    serve(TransLogger(application), host=host, port=port)
 
 """Imports for nuitka"""
 import fractions
