29b4d4bd8056abf42d17f1628cf03c91cdf3df87
Devil666face <artem1999k@gmail.com>
2023-07-05 12:39:42 +0300

    add docker-compose for psql

--- .dev/migrate.sh
+++ .dev/migrate.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+# sudo -u postgres 
+PSQL_PREFIX="psql -E -d db -U superuser -h localhost -c"
 # rm -rf app/migrations/*
 # ./venv/bin/python manage.py makemigrations app
-PSQL_PREFIX="sudo -u postgres psql -E -d db -c"
 source .env
 if [[ "$PSQL" = "True" ]]; then
 	$PSQL_PREFIX "DROP SCHEMA public CASCADE;"
