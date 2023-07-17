a27c576f2e09d8da27c3e9c37780ee784e73ef09
Devil666face <artem1999k@gmail.com>
2023-07-17 21:29:22 +0300

    build.sh

--- .dev/docker-build.sh
+++ .dev/docker-build.sh
@@ -1,6 +1,11 @@
 #/bin/bash
-SQLITE_BUILD=True
+# SQLITE_BUILD=False
 REPO_NAME="${PWD##*/}"
 REPO_NAME_LOWER="$(echo "$REPO_NAME" | tr '[:upper:]' '[:lower:]')"
+if [ "$SQLITE_BUILD" == "True" ]; then
+  echo "SQLITE_BUILD=$SQLITE_BUILD Build with sqlite database"
+else
+  echo "SQLITE_BUILD=$SQLITE_BUILD Build with manage.bin"
+fi
 docker build . -t $REPO_NAME_LOWER
 docker run --rm -v $PWD/dist:/$REPO_NAME/dist -e SQLITE_BUILD=$SQLITE_BUILD --name $REPO_NAME_LOWER-build $REPO_NAME_LOWER
